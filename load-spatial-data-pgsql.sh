ca03ba4395d93a674281d3f52eb0613e0f7e9273
Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>
2025-06-22 06:38:32 -0300

    feat: remove deprecated load-spatial-data scripts and psql wrapper

--- load-spatial-data-pgsql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-shp2pgsql \
-    -D \
-    -I \
-    -s 26918 \
-    postgis-workshop/data/2000/nyc_census_blocks_2000.shp \
-    nyc_census_blocks_2000 \
-    | psql dbname=nyc user=postgres host=localhost 
--- psql.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-podman run -it --rm --network postgis-sandbox_some-network docker.io/postgis/postgis:17-3.5-alpine psql -h some-postgis -U postgres
